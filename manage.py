77193b40733e78baec2f07a79af2e68ce3752099
BroAZIK <124960430+BroAZIK@users.noreply.github.com>
2024-05-01 07:48:52 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -15,10 +15,10 @@ dispatcher = updater.dispatcher
 def register_handlers():
 
     dispatcher.add_handler(CommandHandler("start", start)),
+    dispatcher.add_handler(MessageHandler(Filters.text("Ortga🔙"), back)),
     dispatcher.add_handler(CallbackQueryHandler(button_callback)),
     dispatcher.add_handler(MessageHandler(Filters.text("E'lon joylash ➕📱"), add_phone)),
     dispatcher.add_handler(MessageHandler(Filters.text("Mening e'lonlarim📣"), my_phones)),
-    dispatcher.add_handler(MessageHandler(Filters.text("Ortga🔙"), back)),
     dispatcher.add_handler(MessageHandler(Filters.text("Bot haqida📊"), stats)),
     dispatcher.add_handler(MessageHandler(Filters.text, text)),
     dispatcher.add_handler(MessageHandler(Filters.photo, photo)),
